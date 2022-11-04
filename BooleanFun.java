724027a1fe8901d4b7b1fa5cd323924b5abb496a
Dylan Nguyen <nguyend@wlhs.wlwv.k12.or.us>
2022-11-04 08:57:57 -0700

    Added spaces between messages for ease of use

--- BooleanFun.java
+++ BooleanFun.java
@@ -11,6 +11,7 @@ public class BooleanFun {
         System.out.println("Current age: " + myAge);
 
         //convert these into something more complete using if statements
+        System.out.println ();
         System.out.println("Can rent a car: " + (myAge >= 25));
         if(myAge >= 25){
             System.out.println("You can rent a car!");
@@ -20,6 +21,7 @@ public class BooleanFun {
             System.out.println("You can't rent a car :( ");
         }
 
+        System.out.println ();
         System.out.println("Gets a senior discount: "+ (myAge >= 65));
         if(myAge >= 65){
             System.out.println("You can get a senior discount. You're old lmao.");
@@ -29,6 +31,7 @@ public class BooleanFun {
             System.out.println("You can't yet have a senior discount. One day...");
         }
 
+        System.out.println ();
         System.out.println("Can order from children's menu: " + (myAge <=12));
         if(myAge <= 12){
             System.out.println("You can order from the children's menu!");
@@ -38,7 +41,8 @@ public class BooleanFun {
             System.out.println("Cheap ass. No kids menu for you.");
         }
 
-        System.out.println("On your next birthday out you will be: " + nextAge); //complete this code, don't do math in sout
+        System.out.println ();
+        System.out.println("On your next birthday out you will be: " + nextAge); //complete this code, don't do math inside
 
         //further modification: prompt user for input and then print what they are able to do based on their age
         //and print their age on their next birthday
